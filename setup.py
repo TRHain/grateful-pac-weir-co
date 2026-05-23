31d7a83a1312a377fccb34e2e36c25daf190a02b
Trevor Ryan <43123749+TRHain@users.noreply.github.com>
2026-05-23 10:10:08 -0500

    Create setup.py
    Core setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+# --- Core Setup ---
+import pygame, random, numpy as np, tkinter as tk, json, PIL.Image as Image
+from tkinter import messagebox
+from datetime import datetime
+
+# DEAD-Themed Colors
+DEAD_COLORS = ["gold", "dark_orange", "navy", "green", "red", "black"]
+
+# Global Save File
+SAVE_FILE = "deadpacman_field_notes.json"
